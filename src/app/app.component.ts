825618ae8a245b587a3854ddf0dbb1640075bc6c
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2024-05-15 13:09:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { HighlightDirective } from './highlight.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, HighlightDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'DirectiveExample';
+
+  videos = [
+    {title: 'My video 1', share: 415, likes: 257, dislikes: 12, thumbnail: 'assets/images/image1.jpeg'},
+    {title: 'My video 2', share: 215, likes: 325, dislikes: 12, thumbnail: 'assets/images/image2.jpeg'},
+    {title: 'My video 3', share: 513, likes: 105, dislikes: 12, thumbnail: 'assets/images/image3.jpeg'}
+  ]
+
+  mostLikedVideo = this.getmostLikedVideo();
+
+  getmostLikedVideo(){
+    let videoCopy = [...this.videos];
+    return videoCopy.sort((curr, next) => next.likes - curr.likes)[0];
+  }
 }
